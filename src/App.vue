98f2f1c9b111b73ce2fb6568cfdc1f1602937ba3
Sanchit Agarwal <41899776+sanchitno1@users.noreply.github.com>
2021-06-10 13:45:58 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <NavigationBar/>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/brazil">Brazil</router-link> |
+      <router-link to="/jamaica">Jamaica</router-link> |
+      <router-link to="/hawaii">Hawai</router-link> |
+      <router-link to="/panama">Panama</router-link> |
+      <router-link to="/about">About</router-link> for testing -->
+    </div>
+    <router-view />
+  </div>
+</template>
+<script>
+ import NavigationBar from "@/Components/NavigationBar";
+ export default{
+   components: {
+     NavigationBar
+   }
+ }
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 0 10px;
+}
+</style>
